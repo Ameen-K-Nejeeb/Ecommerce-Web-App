eb035378e6e4e3548483910d8626bb5bcd5339d5
DUDEamee <ameenameen8701@gmail.com>
2025-04-16 13:25:44 +0530

    update : rest_framework

---
+++ shop_app/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Product
+from .models import seri
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "slug", "image", "description", "category", "price"]
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+@api_view({"GET"})
+def products(request):
+    products = products.objects.all()
+    serializer = ProductSerializer(products, )
